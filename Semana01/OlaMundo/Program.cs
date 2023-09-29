e455c7fc05ee252d9a63569dc706d36a39707030
catarina <catirina03@gmail.com>
2023-09-29 10:21:39 +0100

    adicionar comentarios ao programa OlaMundo

--- Semana01/OlaMundo/Program.cs
+++ Semana01/OlaMundo/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+/// <summary>
+/// Este programa imprime argumentos passados pelo ultilizador na linha de comandos.
+/// </summary>
+
 
 namespace OlaMundo
 {
     class Program
     {
+        /// <summary>
+        /// Este é responsavel por imprimir os argumentos
+        /// </summary>
+        
         static void Main(string[] args)
         {
+            //imprime a mensagem
+            
             Console.WriteLine("Hello World!");
         }
     }
