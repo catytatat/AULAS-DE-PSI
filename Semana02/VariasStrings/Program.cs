3eaa3cf9207e4d6f87d264e81a3142863f6c7eba
catarina <catirina03@gmail.com>
2023-10-09 08:31:13 +0100

    Adicionar solução para semana 2

--- Semana02/VariasStrings/Program.cs
+++ Semana02/VariasStrings/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Text;
 
 namespace VariasStrings
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;    
+
+        string s = "Uma string";
+        string t = "Outra string";
+        string r = @"""Uma string entre aspas""";
+        string i = @"\Uma string sem aspas\";
+        string n = "\u00A9 e \u2665";
+        string g = @"\u00A9 e \u2665 sem UTF";
+
+    // Concatenação
+       
+        string cs = "pretinho" + "Teamo" +2;
+        string ct = r + c + x;
+
+    // Formatação com Console.WriteLine
+        Console.WriteLine{"Valor de {0} mais "}
+            Console.WriteLine($"{dd:f2})");   
+            Console.WriteLine($"{dd:p1})");   
+            Console.WriteLine($"{ii:x})");   
+            Console.WriteLine($"{ii:c})");   
+            
+
+    
+    
+
+
+            Console.WriteLine(s);
+            Console.WriteLine(t);
+            Console.WriteLine(r);
+            Console.WriteLine(i);
+            Console.WriteLine(n);
+            Console.WriteLine(g);
+
         }
     }
 }
